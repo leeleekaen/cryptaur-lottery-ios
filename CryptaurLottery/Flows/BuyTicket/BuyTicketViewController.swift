d3f6626841125bb1bab16aa548605d3c2e58909f
kirill shakhansky <bb4bbb@gmail.com>
2018-04-18 17:56:38 +0300

    change background and color by tapping number

--- CryptaurLottery/Flows/BuyTicket/BuyTicketViewController.swift
+++ CryptaurLottery/Flows/BuyTicket/BuyTicketViewController.swift
@@ -3,13 +3,19 @@ import UIKit
 class BuyTicketViewController: UIViewController {
 
     // MARK: - IBAction
-
     @IBAction func clear(_ sender: UIButton) {
         print("Clear button pressed")
     }
     
     @IBAction func numpadAction(_ sender: UIButton) {
         print("\(sender.titleLabel?.text ?? "") is tapped")
+        if sender.currentBackgroundImage == nil {
+            sender.setBackgroundImage(#imageLiteral(resourceName: "number-button-background"), for: .normal)
+            sender.setTitleColor(.white, for: .normal)
+        } else {
+            sender.setBackgroundImage(nil, for: .normal)
+            sender.setTitleColor(.twilight, for: .normal)
+        }
     }
     
     @IBAction func buyAction(_ sender: UIButton) {
