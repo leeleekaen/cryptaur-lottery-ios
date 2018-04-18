e4bc2ed48c461127de671d11852b2ba383b940bf
kirill shakhansky <bb4bbb@gmail.com>
2018-04-18 17:07:50 +0300

    add buy ticket container view

---
+++ CryptaurLottery/Flows/BuyTicket/BuyTicketViewController.swift
@@ -0,0 +1,17 @@
+import UIKit
+
+class BuyTicketViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        configureSubviews()
+    }
+}
+
+// MARK: - Private methods
+private extension BuyTicketViewController {
+    
+    func configureSubviews() {
+        view.backgroundColor = UIColor.paleLavender
+    }
+}
