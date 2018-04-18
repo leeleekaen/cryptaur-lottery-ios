2e6e087f99a7a5505552d61a473c4e6b06c86f31
kirill shakhansky <bb4bbb@gmail.com>
2018-04-18 17:31:07 +0300

    add numpad

--- CryptaurLottery/Flows/BuyTicket/BuyTicketViewController.swift
+++ CryptaurLottery/Flows/BuyTicket/BuyTicketViewController.swift
@@ -8,6 +8,10 @@ class BuyTicketViewController: UIViewController {
         print("Clear button pressed")
     }
     
+    @IBAction func numpadAction(_ sender: UIButton) {
+        print("\(sender.titleLabel?.text ?? "") is tapped")
+    }
+    
     // MARK: - ViewController lifecycle
     override func viewDidLoad() {
         super.viewDidLoad()
