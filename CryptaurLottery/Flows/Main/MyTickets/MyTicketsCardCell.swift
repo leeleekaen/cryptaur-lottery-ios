4a7356350b294d1d344601ebc09fb4b5bd006097
kirill shakhansky <bb4bbb@gmail.com>
2018-04-19 15:03:07 +0300

    add bottom labels to cell

--- CryptaurLottery/Flows/Main/MyTickets/MyTicketsCardCell.swift
+++ CryptaurLottery/Flows/Main/MyTickets/MyTicketsCardCell.swift
@@ -3,15 +3,24 @@ import UIKit
 class MyTicketsCardCell: UICollectionViewCell {
     
     @IBOutlet var numbers: [UIButton]!
+    @IBOutlet weak var timeLeftLabel: UILabel!
+    @IBOutlet weak var dateLabel: UILabel!
+    @IBOutlet weak var wonLabel: UILabel!
     
     func configure(for state: MyTicketsViewController.State) {
         
         switch state {
         case .active:
+            timeLeftLabel.isHidden = false
+            dateLabel.isHidden = true
+            wonLabel.isHidden = true
             numbers.forEach { $0.setBackgroundImage(#imageLiteral(resourceName: "number-button-background"), for: .normal) }
         case .played:
+            timeLeftLabel.isHidden = true
+            dateLabel.isHidden = false
+            wonLabel.isHidden = false
             numbers.forEach { $0.setBackgroundImage(#imageLiteral(resourceName: "loss_number-backgorund"), for: .normal) }
-            numbers[2].setBackgroundImage(#imageLiteral(resourceName: "win-number-background"), for: .normal)
+            numbers[1].setBackgroundImage(#imageLiteral(resourceName: "win-number-background"), for: .normal)
         }
     }
 }
