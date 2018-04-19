8e9982e7d3b6387022afca396deeb86592c5c5b4
Tim S <tima-s@ya.ru>
2018-04-19 05:35:44 +0300

    Menu screen was added

---
+++ CryptaurLottery/Flows/Menu/Screens/MenuViewController.swift
@@ -0,0 +1,47 @@
+//
+//  MenuViewController.swift
+//  CryptaurLottery
+//
+//  Created by Tim S on 19.04.2018.
+//  Copyright © 2018 Nordavind. All rights reserved.
+//
+
+import UIKit
+
+class MenuViewController: BaseViewController {
+    // MARK: - IBOutlets
+    
+    @IBOutlet weak var purseButton: UIButton!
+    @IBOutlet weak var avatarImageView: UIImageView!
+    @IBOutlet weak var loginNameLabel: UILabel!
+    
+    // MARK: - IBAction
+    
+    @IBAction func purseButtonAction(_ sender: Any) {
+        print("Purse button tapped");
+    }
+    
+    @IBAction func logoutButtonAction(_ sender: Any) {
+        print("Logout button tapped");
+    }
+    
+    @IBAction func ticketsButtonAction(_ sender: Any) {
+        print("Tickets button tapped");
+    }
+    
+    @IBAction func changePinButtonAction(_ sender: Any) {
+        print("ChangePin button tapped");
+    }
+    
+    @IBAction func howToPlayButtonAction(_ sender: Any) {
+        print("HowToPlay button tapped");
+    }
+    
+    
+    // MARK: - ViewController lifecycle
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+    }
+    
+}
