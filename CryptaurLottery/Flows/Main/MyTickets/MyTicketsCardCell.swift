e9b562905c21ec7977d1fee17901fc1f49223571
kirill shakhansky <bb4bbb@gmail.com>
2018-04-19 14:17:41 +0300

    change state of numbers when press segment control

--- CryptaurLottery/Flows/Main/MyTickets/MyTicketsCardCell.swift
+++ CryptaurLottery/Flows/Main/MyTickets/MyTicketsCardCell.swift
@@ -2,13 +2,22 @@ import UIKit
 
 class MyTicketsCardCell: UICollectionViewCell {
     
+    @IBOutlet var numbers: [UIButton]!
+    
     func configure(for state: MyTicketsViewController.State) {
         
         switch state {
         case .active:
             print("active")
+            numbers.forEach {
+                $0.setBackgroundImage(#imageLiteral(resourceName: "number-button-background"), for: .normal)
+            }
         case .played:
             print("played")
+            numbers.forEach {
+                $0.setBackgroundImage(#imageLiteral(resourceName: "loss_number-backgorund"), for: .normal)
+            }
+            numbers[2].setBackgroundImage(#imageLiteral(resourceName: "win-number-background"), for: .normal)
         }
     }
 }
