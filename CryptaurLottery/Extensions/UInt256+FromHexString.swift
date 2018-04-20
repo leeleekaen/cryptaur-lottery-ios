d913bbfaf278436e63f132aa59780cfe31dccda0
Alexander Polyakov <eradicator.kai@gmail.com>
2018-04-20 16:41:20 +0400

    UInt256 as string with dots and commas

--- CryptaurLottery/Extensions/UInt256+FromHexString.swift
+++ CryptaurLottery/Extensions/UInt256+FromHexString.swift
@@ -58,4 +58,41 @@ extension UInt256 {
         
         self.init(components)
     }
+    
+    func toString() -> String  {
+        let words = self.words
+        var retVal = ""
+        for word in words {
+            retVal = retVal + "\(word)"
+        }
+        return retVal
+    }
+    
+    func toStringWithDelimeters() -> String  {
+        let s = toString()
+        let endIndex = s.endIndex
+        var retVal = "." + s[s.index(endIndex, offsetBy: -8)...]
+        var offset = -8
+        var addComma = false
+        var previousLeftIndex: String.Index? = nil
+        var digitsCount = 8
+        repeat {
+            offset -= 3
+            if let leftIndex = s.index(endIndex, offsetBy: offset, limitedBy: s.startIndex) {
+                let prepend = s[leftIndex..<s.index(endIndex, offsetBy: offset + 3)]
+                retVal = String(prepend) + (addComma ? "," : "") + retVal
+                previousLeftIndex = leftIndex
+                digitsCount += 3
+            }
+            else {
+                guard previousLeftIndex != nil else {
+                    return s
+                }
+                let prepend = s[s.startIndex..<s.index(s.startIndex, offsetBy: s.count - digitsCount)]
+                retVal = String(prepend) + (addComma ? "," : "") + retVal
+            }
+            addComma = true
+        } while -offset < s.count
+        return retVal
+    }
 }
