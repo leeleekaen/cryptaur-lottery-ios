ec05c1181564ec400d6a6cc055e1e7f20107815e
kirill shakhansky <bb4bbb@gmail.com>
2018-04-20 12:22:41 +0300

    add winAmount to viewModel and initialize it with fake data

--- CryptaurLottery/Flows/Main/MyTickets/MyTicketsViewModel.swift
+++ CryptaurLottery/Flows/Main/MyTickets/MyTicketsViewModel.swift
@@ -1,4 +1,18 @@
 import Foundation
 
 class MyTicketsViewModel: BaseViewModel {
+    
+    var winAmount: WinAmount?
+    
+    override init() {
+        super.init()
+        
+        let json = getWinAmountJson()
+        winAmount = WinAmount(json: json)
+    }
+    
+    private func getWinAmountJson() -> JSONDictionary {
+        return ["winAmount": "0x203040",
+                "pickUpWinGasFee": "0x203040"]
+    }
 }
