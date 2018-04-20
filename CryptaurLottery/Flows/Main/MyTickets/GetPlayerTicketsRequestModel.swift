0d84bac766193b62c20ef82c2359e1d792704048
kirill shakhansky <bb4bbb@gmail.com>
2018-04-20 16:59:04 +0300

    add GetPlayerTickets request and responce model

---
+++ CryptaurLottery/Flows/Main/MyTickets/GetPlayerTicketsRequestModel.swift
@@ -0,0 +1,9 @@
+//
+//  GetPlayerTicketsRequestModel.swift
+//  CryptaurLottery
+//
+//  Created by Mac Mini on 20/04/2018.
+//  Copyright © 2018 Nordavind. All rights reserved.
+//
+
+import Foundation
---
+++ CryptaurLottery/Flows/Main/MyTickets/GetPlayerTicketsResponceModel.swift
@@ -0,0 +1,9 @@
+//
+//  GetPlayerTicketsResponceModel.swift
+//  CryptaurLottery
+//
+//  Created by Mac Mini on 20/04/2018.
+//  Copyright © 2018 Nordavind. All rights reserved.
+//
+
+import Foundation
