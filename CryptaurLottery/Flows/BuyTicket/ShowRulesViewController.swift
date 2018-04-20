2773db6ea06415b80f3be0c2ec99b10022adeffd
Tim S <tima-s@ya.ru>
2018-04-20 15:07:33 +0300

    delete commented code

--- CryptaurLottery/Flows/BuyTicket/ShowRulesViewController.swift
+++ CryptaurLottery/Flows/BuyTicket/ShowRulesViewController.swift
@@ -8,7 +8,7 @@
 
 import UIKit
 
-class ShowRulesViewController: UIViewController {
+class ShowRulesViewController: BaseViewController {
 
     @IBOutlet weak var rulesTextView: UITextView!
 
@@ -45,20 +45,5 @@ class ShowRulesViewController: UIViewController {
         configureText()
     }
 
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
-    }
-    
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destinationViewController.
-        // Pass the selected object to the new view controller.
-    }
-    */
 
 }
