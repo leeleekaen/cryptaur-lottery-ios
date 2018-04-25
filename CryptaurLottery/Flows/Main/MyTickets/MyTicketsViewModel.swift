7154c16232cfb9c1c0e9fff10f398ae67f42d58f
kirill shakhansky <bb4bbb@gmail.com>
2018-04-25 14:00:18 +0300

    refactor

--- CryptaurLottery/Flows/Main/MyTickets/MyTicketsViewModel.swift
+++ CryptaurLottery/Flows/Main/MyTickets/MyTicketsViewModel.swift
@@ -6,29 +6,41 @@ import UInt256
 class MyTicketsViewModel: BaseViewModel {
     
     // MARK: - Public properties
-    var activeTickets = [Ticket]() {
-        didSet {
-            print("number of tickets \(activeTickets.count)")
-        }
+    var allActiveTickets: [Ticket] { return activeTickets.values.reduce([Ticket]()) { $0 + $1 } }
+    var allPlayedTickets: [Ticket] { return playedTickets.values.reduce([Ticket]()) { $0 + $1 } }
+    
+    var updateCompletion: (() -> ())?
+    var isLoading: Driver<Bool> {
+        return isLoadingSubject.asDriver(onErrorJustReturn: false)
     }
-    var playedTickets = [Ticket]() {
-        didSet {
-            print("number of tickets \(playedTickets.count)")
-        }
-    }
+    
     var winAmount: Driver<UInt256> {
         return winAmountSubject.asDriver(onErrorJustReturn: UInt256(integerLiteral: 0))
     }
     
-    
     // MARK: - Private properties
     private let lotteries: [LotteryID] = [.lottery4x20, .lottery5x36, .lottery6x42]
     private let playerAddress = UInt256(hexString: "0x14f05a4593ee1808541525a5aa39e344381251e6")!
-    private let winAmountSubject = BehaviorSubject<UInt256>(value: UInt256(integerLiteral: 0))
-    private let ticketsUpdaterSubject = BehaviorSubject<[Ticket]>(value: [])
-    private var ticketsUpdater: Driver<[Ticket]> {
-        return ticketsUpdaterSubject.asDriver(onErrorJustReturn: [])
+    
+    private var activeTickets: [LotteryID: [Ticket]] = [.lottery4x20: [], .lottery5x36: [], .lottery6x42: []]
+    private var playedTickets: [LotteryID: [Ticket]] = [.lottery4x20: [], .lottery5x36: [], .lottery6x42: []] {
+        didSet {
+            print(allPlayedTickets.count)
+        }
     }
+    
+    private var loadingCount = 0 {
+        didSet {
+            if loadingCount == 0 {
+                isLoadingSubject.onNext(false)
+            } else {
+                isLoadingSubject.onNext(true)
+            }
+        }
+    }
+    private let isLoadingSubject = BehaviorSubject<Bool>(value: false)
+    
+    private let winAmountSubject = BehaviorSubject<UInt256>(value: UInt256(integerLiteral: 0))
     
     // MARK: - Dependency
     private let winAmountService = GetWinAmountService()
@@ -38,21 +50,16 @@ class MyTicketsViewModel: BaseViewModel {
     // MARK: - Lifecycle
     override init() {
         super.init()
-        
-        ticketsUpdater.drive(onNext: { [weak self] (tickets) in
-            print("add new \(tickets.count) tickets")
-            self?.activeTickets += tickets
-        }).disposed(by: disposeBag)
-        
         update(for: playerAddress, and: lotteries)
     }
     
+    // MARK: - Get data from server
     func update(for playerAddress: UInt256, and lotteries: [LotteryID]) {
         
         updateWinAmount(for: playerAddress)
         
         lotteries.forEach {
-            updateTickets(for: playerAddress, and: $0)
+            updateTickets(playerAddress: playerAddress, lottery: $0, offset: 0, count: 5)
         }
     }
     
@@ -68,20 +75,33 @@ class MyTicketsViewModel: BaseViewModel {
 // MARK: - Private methods
 private extension MyTicketsViewModel {
     
-    func updateTickets(for playerAddress: UInt256, and lottery: LotteryID) {
+    func updateTickets(playerAddress: UInt256, lottery: LotteryID, offset: UInt, count: UInt) {
         
-        print("Start update tickets for lottery: \(lottery)")
+        loadingCount += 1
+        
         let requestModel = GetPlayerTicketsRequestModel(playerAddress: playerAddress,
                                                         lotteryID: lottery,
-                                                        offset: 0,
-                                                        count: 10)
+                                                        offset: offset,
+                                                        count: count)
         
         playerTicketsService.perform(input: requestModel,
                                      success: { [weak self] (responce) in
                                         print("succes respond for \(lottery) count: \(responce.tickets.count)")
-                                        self?.ticketsUpdaterSubject.onNext(responce.tickets)
-        }) { (error) in
+                                        var activeTickets = [Ticket]()
+                                        var playedTickets = [Ticket]()
+                                        responce.tickets.forEach {
+                                            if $0.winLevel == -1 {
+                                                activeTickets.append($0)
+                                            } else {
+                                                playedTickets.append($0)
+                                            }
+                                        }
+                                        self?.activeTickets[lottery]! += activeTickets
+                                        self?.playedTickets[lottery]! += playedTickets
+                                        self?.loadingCount -= 1
+        }) { [weak self] (error) in
             print("Error for lottery \(lottery): \(error)")
+            self?.loadingCount -= 1
         }
     }
     
