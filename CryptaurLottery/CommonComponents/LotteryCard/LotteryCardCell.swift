86a891163152021244442353051f0cafe7e6ae14
kirill shakhansky <bb4bbb@gmail.com>
2018-04-26 15:56:31 +0300

    configure label with draw data exept time left label

--- CryptaurLottery/CommonComponents/LotteryCard/LotteryCardCell.swift
+++ CryptaurLottery/CommonComponents/LotteryCard/LotteryCardCell.swift
@@ -1,8 +1,16 @@
 import UIKit
+import UInt256
 
 class LotteryCardCell: UICollectionViewCell {
     
     @IBOutlet weak var ballsImage: UIImageView!
+    @IBOutlet weak var drawIndexLabel: UILabel!
+    @IBOutlet weak var jackpotLabel: UILabel!
+    @IBOutlet weak var buyTicketButton: UIButton! {
+        didSet {
+            buyTicketButton.titleLabel?.adjustsFontSizeToFitWidth = true
+        }
+    }
     
     func configure(draw: Draw) {
         
@@ -16,5 +24,19 @@ class LotteryCardCell: UICollectionViewCell {
         case .lottery6x42:
             ballsImage.image = #imageLiteral(resourceName: "balls 6x42 large")
         }
+        
+        drawIndexLabel.text = "DRAW #\(draw.number)"
+        
+        var jackpot = draw.jackpot.toStringWithDelimeters()
+        if draw.jackpot != UInt256(integerLiteral: 0) {
+            jackpot.removeLast(5)
+        }
+        jackpotLabel.text = jackpot + " CPT"
+        
+        var ticketPrice = draw.ticketPrice.toStringWithDelimeters()
+        if draw.ticketPrice != UInt256(integerLiteral: 0) {
+            ticketPrice.removeLast(5)
+        }
+        buyTicketButton.setTitle("Buy ticket for \(ticketPrice) CPT", for: .normal)
     }
 }
