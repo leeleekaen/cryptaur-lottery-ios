391f12175d4afce9fd38e653acd290b63e4843a3
kirill shakhansky <bb4bbb@gmail.com>
2018-04-26 15:28:36 +0300

    configure image in lottery cell

--- CryptaurLottery/CommonComponents/LotteryCard/LotteryCardCell.swift
+++ CryptaurLottery/CommonComponents/LotteryCard/LotteryCardCell.swift
@@ -1,16 +1,20 @@
-//
-//  LotteryCardView.swift
-//  CryptaurLottery
-//
-//  Created by Alexander Polyakov on 07.04.2018.
-//  Copyright © 2018 Nordavind. All rights reserved.
-//
-
 import UIKit
 
 class LotteryCardCell: UICollectionViewCell {
     
+    @IBOutlet weak var ballsImage: UIImageView!
+    
     func configure(draw: Draw) {
-        print(draw)
+        
+        guard let lottery = LotteryID(rawValue: draw.lotteryID) else { return }
+        
+        switch lottery {
+        case .lottery4x20:
+            ballsImage.image = #imageLiteral(resourceName: "balls 4x20 large")
+        case .lottery5x36:
+            ballsImage.image = #imageLiteral(resourceName: "balls-5x36 large")
+        case .lottery6x42:
+            ballsImage.image = #imageLiteral(resourceName: "balls 6x42 large")
+        }
     }
 }
