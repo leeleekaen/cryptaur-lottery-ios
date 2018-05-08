735a7f840ec4a8e38e1988e6e1594afa94a15d2a
kirill shakhansky <bb4bbb@gmail.com>
2018-05-08 17:27:47 +0300

    fix pickUpWin request

--- CryptaurLottery/Flows/Main/MyTickets/Services/PickUpWinServicel.swift
+++ CryptaurLottery/Flows/Main/MyTickets/Services/PickUpWinServicel.swift
@@ -1,4 +1,5 @@
 import Foundation
+import Alamofire
 
 final class PickUpWinService:
 OperationService<PickUpWinRequestModel, PickUpWinResponceModel> {
@@ -19,12 +20,28 @@ OperationService<PickUpWinRequestModel, PickUpWinResponceModel> {
 
 fileprivate final class GetPlayerTicketsOperation: APIOperation {
     
+    override func createRequest() -> DataRequest {
+        
+        return Alamofire.request(URL(with: endpoint),
+                                 method: endpoint.method.asAlamofireMethod(),
+                                 parameters: parameters,
+                                 encoding: JSONEncoding.default,
+                                 headers: headers)
+    }
+    
     init(request: PickUpWinRequestModel,
          success: @escaping APIOperationSuccess,
          failure: @escaping ServiceFailure) {
         
         let parameters = ["playerAddress": "\(request.playerAddress.normalizedHexString)"]
         
-        super.init(endpoint: .connect, parameters: parameters, headers: ["Authorization": "Bearer \(request.authKey)"], success: success, failure: failure)
+        let headers = [
+            "Authorization": "Bearer \(request.authKey)",
+            "Content-Type": "application/json"
+        ]
+        
+        super.init(endpoint: .pickUpWin,
+                   parameters: parameters, headers: headers,
+                   success: success, failure: failure)
     }
 }
